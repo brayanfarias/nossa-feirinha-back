317a2403ad8b428d2329f50f85598d15400f57e3
bpjfarias <bpjfarias@gmail.com>
2020-09-22 14:06:23 -0300

    criei o controller, as rotas, o data e o readme do data

--- src/routes.ts
+++ src/routes.ts
@@ -1,6 +1,7 @@
 
 import express = require('express');
 import consumidorController from "./controller/consumidorController";
+import produtorController from "./controller/produtorController";
 
 const routes = express.Router();
 
@@ -8,5 +9,10 @@ routes.post('/consumidor', new consumidorController().create);
 routes.get('/consumidor/:email', new consumidorController().getByEmail);
 routes.patch('/consumidor', new consumidorController().update)
 routes.delete('/consumidor/:idUsuario', new consumidorController().delete)
+
+routes.post('/produtor', new produtorController().create);
+routes.get('/produtor/:email', new produtorController().getByEmail);
+routes.patch('/produtor', new produtorController().update)
+routes.delete('/produtor/:idUsuario', new produtorController().delete)
 
 export default routes;
