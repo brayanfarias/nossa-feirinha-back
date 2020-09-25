dba556bfd2940e2d3e79c0bbd8b5470efdc03eb3
bpjfarias <bpjfarias@gmail.com>
2020-09-25 15:56:38 -0300

    adicionei atributo descricao e Objeto Endereco

--- src/entity/Evento.ts
+++ src/entity/Evento.ts
@@ -1,5 +1,6 @@
 import { Entity, PrimaryGeneratedColumn, Column, JoinColumn, ManyToOne, OneToOne, ViewColumn, JoinTable } from "typeorm";
 import { Consumidor } from "./Consumidor";
+import { Endereco } from "./Endereco";
 import { Produtor } from "./Produtor";
 
 @Entity()
@@ -12,21 +13,36 @@ export class Evento {
     nome: string;
 
     @Column()
+    descricao: string;
+
+    @Column()
     dataEvento: string;
 
     @Column()
     latitude: string;
 
     @Column()
-    longitude: string
-    
-    @ManyToOne(type => Consumidor, consumidor => consumidor.eventos,{
+    longitude: string;
+
+    @ManyToOne(type => Consumidor, consumidor => consumidor.eventos, {
         eager: true,
-    })  
+    })
     criadorConsumidor: Consumidor;
 
     @ManyToOne(type => Produtor, produtor => produtor.eventos, {
         eager: true,
     })
     criadorProdutor: Produtor;
+
+    @OneToOne(type => Endereco, {
+        cascade: true,
+        /**
+         * @todo Implementar outra forma de deletar a linha do Endereco
+         * @see https://github.com/typeorm/typeorm/issues/3218
+         */
+        onDelete: "CASCADE",
+        eager: true
+    })
+    @JoinColumn()
+    endereco: Endereco;
 }
