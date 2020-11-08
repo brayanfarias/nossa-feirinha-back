6eb5d4e57d968398e35e06a663505ec3e4d5f2e0
bpjfarias <bpjfarias@gmail.com>
2020-11-08 19:31:44 -0300

    add updateEvento e removendo role p get de eventos

--- src/routes.ts
+++ src/routes.ts
@@ -1,17 +1,16 @@
-
 import { Router } from "express";
-import EventoController from "./controller/EventoController";
-import ConsumidorController from "./controller/ConsumidorController";
-import ProdutorController from "./controller/ProdutorController";
-import ProdutoController from './controller/ProdutoController';
-import GondolaController from './controller/GondolaController';
-import AssinaturaController from './controller/AssinaturaController';
-import SessionController from "./controller/SessionController";
-import PermissionController from "./controller/PermissionController";
-import RoleController from "./controller/RoleController";
-import { is } from "./middleware/permissions";
 import AdministradorController from "./controller/AdministradorController";
-import ExposicaoController from "./controller/ExposicaoController"
+import AssinaturaController from './controller/AssinaturaController';
+import ConsumidorController from "./controller/ConsumidorController";
+import EventoController from "./controller/EventoController";
+import ExposicaoController from "./controller/ExposicaoController";
+import GondolaController from './controller/GondolaController';
+import PermissionController from "./controller/PermissionController";
+import ProdutoController from './controller/ProdutoController';
+import ProdutorController from "./controller/ProdutorController";
+import RoleController from "./controller/RoleController";
+import SessionController from "./controller/SessionController";
+import { is } from "./middleware/permissions";
 
 const routes = Router();
 
@@ -34,7 +33,8 @@ routes.delete('/produtor/:idUsuario',is(["ROLE_ADMIN"]), ProdutorController.dele
 routes.post('/admin',AdministradorController.create)
 
 routes.post('/evento',is(["ROLE_PRODUTOR","ROLE_CONSUMIDOR"]), EventoController.createEvento);
-routes.get('/evento/:idEvento',is(["ROLE_PRODUTOR","ROLE_CONSUMIDOR"]), EventoController.getEvento)
+routes.patch('/evento', is(["ROLE_PRODUTOR","ROLE_CONSUMIDOR"]), EventoController.updateEvento)
+routes.get('/evento/:idEvento', EventoController.getEvento)
 routes.get('/evento', EventoController.getEventosAtivos)
 routes.delete('/evento/:idEvento',is(["ROLE_PRODUTOR","ROLE_CONSUMIDOR"]), EventoController.deleteEventoAndItsRelations)
 routes.get('/evento/:idEvento/get-subscribers',is(["ROLE_PRODUTOR","ROLE_CONSUMIDOR"]), EventoController.getSubscribersAtivos)
