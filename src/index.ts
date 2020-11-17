b8c2b3a42edb8c71995b3da5e7ac3dcbca726364
bpjfarias <bpjfarias@gmail.com>
2020-11-17 12:31:01 -0300

    add Headers

--- src/index.ts
+++ src/index.ts
@@ -11,7 +11,8 @@ createConnection()
 
 app.use((req, res, next) => {
     res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Methods", 'GET,PUT,POST,DELETE,PATCH');
+    res.header("Access-Control-Allow-Methods", 'GET,PUT,POST,DELETE,PATCH, OPTIONS');
+    res.header('Access-Control-Allow-Headers', "*");
     app.use(cors());
     next();
 });
