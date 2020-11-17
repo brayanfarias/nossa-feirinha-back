11120012598911a8052b0bb2b4ea1ce0427c78ba
bpjfarias <bpjfarias@gmail.com>
2020-11-17 00:16:53 -0300

    add urlencoded

--- src/index.ts
+++ src/index.ts
@@ -8,7 +8,9 @@ const app = express();
 
 createConnection()
 
-app.use(bodyParser.json())
+app.use(bodyParser.json({limit: '10mb'}))
+
+app.use(bodyParser.urlencoded({limit: '10mb', extended: true}))
 
 app.use(express.json());
 
